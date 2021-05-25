121018abccca0abbbc95b0860783d77a01688eae
hanifxdp <briandglory@gmail.com>
2021-05-25 14:41:29 +0700

    add new dataset and query

--- connection/MentorConnection.js
+++ connection/MentorConnection.js
@@ -13,7 +13,7 @@ exports.getMentors = async (param) => {
   const queryData = {
     query: `PREFIX data:<http://example.com/>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-    SELECT ?id ?name ?img ?desc ?category
+    SELECT ?id ?name ?img ?desc ?categoryName
     WHERE{
         ?sub rdf:type data:mentor
         OPTIONAL {?sub data:id ?id.}
@@ -21,8 +21,36 @@ exports.getMentors = async (param) => {
         OPTIONAL {?sub data:img ?img.}
         OPTIONAL {?sub data:link ?link.}
         OPTIONAL {?sub data:desc ?desc.}
-        OPTIONAL {?sub data:category ?category.}
+        OPTIONAL {?sub data:onCategory ?categoryID.}
+        OPTIONAL {?categoryID data:categoryName ?categoryName.}
         FILTER regex(?name, "${param.name ? param.name : ""}", "i")
+        FILTER regex(?categoryName, "${param.category ? param.category : ""}", "i")
+    }`,
+  };
+  try {
+    const { data } = await axios(`${DATA_URL}/carimentor/query`, {
+      method: "POST",
+      headers,
+      data: qs.stringify(queryData),
+    });
+    return data.results;
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.getCategory = async (param) => {
+  // Query
+  const queryData = {
+    query: `PREFIX data:<http://example.com/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+
+    SELECT DISTINCT ?categoryID ?categoryName
+    WHERE{
+        ?sub rdf:type data:mentor
+        OPTIONAL {?sub data:id ?id.}
+        OPTIONAL {?sub data:onCategory ?categoryID.}
+        OPTIONAL {?categoryID data:categoryName ?categoryName.}
         FILTER regex(?category, "${param.category ? param.category : ""}", "i")
     }`,
   };
--- controllers/MentorController.js
+++ controllers/MentorController.js
@@ -3,74 +3,74 @@ const formatAll = require("../tools/formatAll");
 const formatCategory = require("../tools/formatCategory");
 
 module.exports = {
-	getMentors : async (req, res) => {
-		try {
-			console.log("function starting");
-			// Query data dari repo
-			let mentors = await connection.getMentors(req.query);
+  getMentors: async (req, res) => {
+    try {
+      console.log("function starting");
+      // Query data dari repo
+      let mentors = await connection.getMentors(req.query);
 
-			if (!mentors.bindings.length) {
-				return res.status(200).json({
-					data: [],
-					message: "Data tidak ditemukan",
-				});
-			}
+      if (!mentors.bindings.length) {
+        return res.status(200).json({
+          data: [],
+          message: "Data tidak ditemukan",
+        });
+      }
 
-			mentors = mentors.bindings.map((mentor) => formatAll(mentor));
+      mentors = mentors.bindings.map((mentor) => formatAll(mentor));
 
-			if (req.params.id) {
-				let mentor = mentors.filter((mentor) => {
-					return mentor.id == req.params.id;
-				});
-				res.status(200).json({
-					data: mentor[0],
-					message: mentor.length
-						? "Data mentor berhasil didapatkan"
-						: "Tidak ada hasil dari pencarian",
-				});
-			} else {
-				res.status(200).json({
-					data: mentors,
-					message: "Show all mentor",
-				});
-			}
-		} catch (err) {
-			res.status(400).json(err);
-		}
-	},
-	getCategory : async (req, res) => {
-		try {
-			console.log("function starting");
-			// Query data dari repo
-			let mentors = await connection.getCategory(req.query);
+      if (req.params.id) {
+        let mentor = mentors.filter((mentor) => {
+          return mentor.id == req.params.id;
+        });
+        res.status(200).json({
+          data: mentor[0],
+          message: mentor.length
+            ? "Data mentor berhasil didapatkan"
+            : "Tidak ada hasil dari pencarian",
+        });
+      } else {
+        res.status(200).json({
+          data: mentors,
+          message: "Show all mentor",
+        });
+      }
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+  getCategory: async (req, res) => {
+    try {
+      console.log("function starting");
+      // Query data dari repo
+      let mentors = await connection.getCategory(req.query);
 
-			if (!mentors.bindings.length) {
-				return res.status(200).json({
-					data: [],
-					message: "Data tidak ditemukan",
-				});
-			}
+      if (!mentors.bindings.length) {
+        return res.status(200).json({
+          data: [],
+          message: "Data tidak ditemukan",
+        });
+      }
 
-			mentors = mentors.bindings.map((mentor) => formatCategory(mentor));
+      mentors = mentors.bindings.map((mentor) => formatCategory(mentor));
 
-			if (req.params.id) {
-				let mentor = mentors.filter((mentor) => {
-					return mentor.id == req.params.id;
-				});
-				res.status(200).json({
-					data: mentor[0],
-					message: mentor.length
-						? "Data mentor berhasil didapatkan"
-						: "Tidak ada hasil dari pencarian",
-				});
-			} else {
-				res.status(200).json({
-					data: mentors,
-					message: "Show all mentor",
-				});
-			}
-		} catch (err) {
-			res.status(400).json(err);
-		}
-	},
-}
+      if (req.params.id) {
+        let mentor = mentors.filter((mentor) => {
+          return mentor.id == req.params.id;
+        });
+        res.status(200).json({
+          data: mentor[0],
+          message: mentor.length
+            ? "Data category berhasil didapatkan"
+            : "Tidak ada hasil dari pencarian",
+        });
+      } else {
+        res.status(200).json({
+          data: mentors,
+          message: "Show all category",
+        });
+      }
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  },
+};
